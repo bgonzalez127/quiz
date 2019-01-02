922b935e84201e438db8bb8bcd0d70f8bacfaebe
Brianna Gonzalez <bgonzalez2019@brooklynprephs.org>
2019-01-02 22:48:35 +0000

    functions and total score calculation

--- js/script.js
+++ js/script.js
@@ -8,7 +8,73 @@ $(document).ready(function() {
         var q1Input = $("#question1").val();
         var q2Input = $("#question2").val();
         var q3Input = $("#question3").val();
-        var totalScore;
+        var totalScore = chooseGenre(q1Input) + chooseAnimal(q2Input) + chooseColor(q3Input);
     });
-
 });
+
+
+function chooseGenre(userInput){
+    if (userInput === "Suspence"){
+        return 1;
+    } else if (userInput === "Action"){
+        return 2;
+    } else if (userInput === "Informational"){
+        return 3;
+    } else if (userInput === "Sports"){
+        return 4;
+    } else{
+        return 0;
+    }
+
+function chooseAnimal(userInput){
+    if (userInput === "Bird"){
+        return 1;
+    } else if (userInput === "Lion"){
+        return 2;
+    } else if (userInput === "Dog"){
+        return 3;
+    } else if (userInput === "Bear"){
+        return 4;
+    } else{
+        return 0;
+    }
+    
+function chooseColor(userInput){
+    if (userInput === "Blue"){
+        return 1;
+    } else if (userInput === "Red"){
+        return 2;
+    } else if (userInput === "Orange"){
+        return 3;
+    } else if (userInput === "Pink"){
+        return 4;
+    } else{
+        return 0;
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
